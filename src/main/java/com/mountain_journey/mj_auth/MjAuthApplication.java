3789c877c06650a125b8c8434fc501cde99ef303
ThibautFgi <thibaut.fgi@hotmail.com>
2026-03-05 11:45:45 +0100

    commit

---
+++ src/main/java/com/mountain_journey/mj_auth/MjAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mountain_journey.mj_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountain_journey/mj_auth/MjAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain_journey.mj_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
