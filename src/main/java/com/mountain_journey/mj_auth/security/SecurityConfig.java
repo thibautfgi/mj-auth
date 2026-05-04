6ecd06131e163ef2b92479ccc5d8a40f65685420
ThibautFgi <thibaut.fgi@hotmail.com>
2026-05-04 16:01:58 +0200

    connection

--- src/main/java/com/mountain_journey/mj_auth/security/SecurityConfig.java
+++ src/main/java/com/mountain_journey/mj_auth/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
